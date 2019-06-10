245081d7b1280dfb89f23c680886f79ee6e7b4a0
XfHua <jasonHuaxf@163.com>
2019-06-10 16:15:05 +0800

    213

---
+++ App396/App396/MainActivity.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Webkit;
+using Android.Widget;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+
+namespace App396
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            Plugin.CurrentActivity.CrossCurrentActivity.Current.Init(this, savedInstanceState);
+
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += FabOnClick;
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            View view = (View) sender;
+
+            CheckMyPermissionAsync();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public event EventHandler<DownloadEventArgs> OnFileDownloaded;
+
+        public void DownloadFile(string url, string folder)
+        {
+            string pathToNewFolder = Path.Combine(Android.OS.Environment.ExternalStorageDirectory.AbsolutePath, folder);
+
+            Directory.CreateDirectory(pathToNewFolder);
+
+            try
+            {
+                WebClient webClient = new WebClient();
+
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                string pathToNewFile = Path.Combine(pathToNewFolder, Path.GetFileName(url));
+
+                webClient.DownloadFileAsync(new Uri(url), pathToNewFile);
+            }
+            catch (Exception ex)
+            {
+                if (OnFileDownloaded != null) { }
+            }
+        }
+
+
+        private void Completed(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                if (OnFileDownloaded != null) { }
+            }
+            else
+            {
+                if (OnFileDownloaded != null) { }
+
+            }
+        }
+
+        public async void CheckMyPermissionAsync()
+        {
+
+            var permissionsStartList = new List<Permission>()
+        {
+            Permission.Storage
+        };
+
+            var permissionsNeededList = new List<Permission>();
+            try
+            {
+                foreach (var permission in permissionsStartList)
+                {
+                    var status = await CrossPermissions.Current.CheckPermissionStatusAsync(permission);
+                    if (status != PermissionStatus.Granted)
+                    {
+                        permissionsNeededList.Add(permission);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+
+            var results = await CrossPermissions.Current.RequestPermissionsAsync(permissionsNeededList.ToArray());
+
+            //Check the persimmison again
+            var storeagePermission = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Storage);
+
+            if (storeagePermission == PermissionStatus.Granted)
+            {
+                //Download file here
+                DownloadFile("http://www.dada-data.net/uploads/image/hausmann_abcd.jpg", "XF_Downloads");
+            }
+            else {
+
+                Console.WriteLine("No permissions");
+            }         
+        }
+    }
+}
+
+
